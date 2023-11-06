9de4c3a83eeefaaff25f32c038158abf5e815ac5
yugesh kanna s <yuki-paul>
2023-11-06 14:43:37 +0530

    correct version updated version(0.01)

---
+++ src/main/java/com/example/blog/Configuration/jwtAuthenticationEntryPoint.java
@@ -0,0 +1,24 @@
+package com.example.blog.Configuration;
+
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class jwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+	public void commence(HttpServletRequest request,
+			HttpServletResponse response,
+			AuthenticationException authException) 
+					throws IOException, ServletException {
+	 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+		
+	}
+
+}
